a23eeb3b93947bb1e2d7256275dff0a97c30e8f9
manjusree-04 <manjusree.github@gmail.com>
2025-01-08 22:14:22 +0530

    1

---
+++ assessment1.py
@@ -0,0 +1,8 @@
+integer_var = 10         
+float_var = 3.14          
+string_var = "Hello, world!"
+boolean_var = True
+print("Value:", integer_var, " Type:", type(integer_var))
+print("Value:", float_var, " Type:", type(float_var))
+print("Value:", string_var, " Type:", type(string_var))
+print("Value:", boolean_var, " Type:", type(boolean_var))
