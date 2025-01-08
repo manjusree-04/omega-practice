af833580953d26dc7369d48690d7279665939ccc
manjusree-04 <manjusree.github@gmail.com>
2025-01-08 22:18:04 +0530

    2

---
+++ assessment2.py
@@ -0,0 +1,9 @@
+my_list = [10, 20, 30, 40, 50]
+print("List Element at index 2:", my_list[2])
+print("List Element at index -1:", my_list[-1])
+my_tuple = (1.5, 2.5, 3.5, 4.5, 5.5)
+print("Tuple Element at index 0:", my_tuple[0])
+print("Tuple Element at index -3:", my_tuple[-3])
+my_dict = {'name': 'Alice', 'age': 25, 'city': 'New York', 'job': 'Engineer', 'country': 'USA'}
+print("Dictionary Element with key 'name':", my_dict['name'])
+print("Dictionary Element with key 'age':", my_dict['age'])
